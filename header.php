e6817ffea350cee149e900f05ce1b77d02fe27d3
halfg33k <johnsonam17@gmail.com>
2019-02-14 11:18:15 -0500

    install hotjar

--- header.php
+++ header.php
@@ -27,6 +27,18 @@
     <![endif]-->
 </head>
 <body>
+
+    <!-- Hotjar Tracking Code for http://dev.childressagency.com/ -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1199441,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
     
     <div class="contact">
         <div class="contact__close" style="z-index: 99"><i class="fas fa-times"></i></div>
